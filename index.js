4bf8c64d6f4dd65abf79716658cfa548db518e8f
Aya Shahal <ayashahal055@gmail.com>
2023-05-08 19:26:44 +0300

    backend structure

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import bodyParser from "body-parser";
+import UserRouter from "./routes/UserRoute.js";
+dotenv.config();
+
+  
+
+  
+await connectDB();
+const PORT = process.env.PORT || 6000;
+const app = express();
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+}
+
+app.get("/", (req, res) => {
+    res.send("API is running ...");
+});
+app.use("/api/user", UserRouter);
+
+app.listen(
+    PORT,
+    console.log(
+        `Server runing in ${process.env.NODE_ENV} mode on port ${PORT}  `
+    )
+);
