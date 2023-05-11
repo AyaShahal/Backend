0a047a4e6554fd891f131e5c92b6b4c8ddd9e739
Aya Shahal <ayashahal055@gmail.com>
2023-05-11 15:45:55 +0300

    category schema

---
+++ models/CategoryModel.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+const { Schema, model } = mongoose;
+
+const categorySchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+      
+        image: {
+            type: String,
+            required: true,
+        },
+        adminUsername: {
+            type: String,
+            required: true,
+        },
+    },
+
+    {
+        collection: "categories",
+        timestamps: true,
+    }
+);
+
+const Category = model("Category", categorySchema);
+export default Category;
