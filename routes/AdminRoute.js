940c9f4cf5b07876ac9c508858f9b9247188f228
Aya Shahal <ayashahal055@gmail.com>
2023-05-11 15:53:32 +0300

    admin route

--- routes/AdminRoute.js
+++ routes/AdminRoute.js
@@ -1,21 +1,23 @@
 import express from "express";
 const router = express.Router();
 import controller from "../controllers/AdminController.js";
-import verifyToken from "../middleware/auth.js";
+import verifyToken, { verifyAdmin } from "../middleware/auth.js";
 
-router.get("/", verifyToken, controller.getAll);
-router.get("/:id", verifyToken,  controller.get);
+router.get("/", verifyToken, verifyAdmin, controller.getAll);
+router.get("/:id", verifyToken, verifyAdmin, controller.get);
 router.post(
     "/register",
-  
+    verifyToken,
+    verifyAdmin,
     controller.Register
 );
 router.post("/login", controller.login);
 router.post("/logout", controller.logOut);
-router.patch("/:id", verifyToken,  controller.edit);
+router.patch("/:id", verifyToken, verifyAdmin, controller.edit);
 router.delete(
     "/:id",
     verifyToken,
+    verifyAdmin,
     controller.deleteadmin
 );
 
