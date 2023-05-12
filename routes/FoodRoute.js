f3d20e8336b8bff9ff4dcc77a6cba441102cdeeb
Aya Shahal <ayashahal055@gmail.com>
2023-05-12 20:39:12 +0300

    food route

---
+++ routes/FoodRoute.js
@@ -0,0 +1,29 @@
+import express from "express";
+import FoodController from "../controllers/FoodController.js";
+import verifyToken, { authenticateBusiness } from "../middleware/auth.js";
+import imageHandle from "../middleware/imageHandle.js";
+
+const router = express.Router();
+
+router.get("/", FoodController.getAllFoods);
+router.get("/:id", FoodController.getFoodById);
+
+router.post(
+  "/",
+  verifyToken,
+  authenticateBusiness,
+  imageHandle,
+  FoodController.addFood
+);
+
+router.patch(
+  "/:id",
+  verifyToken,
+  authenticateBusiness,
+  imageHandle,
+  FoodController.editFood
+);
+
+router.delete("/:id", verifyToken, authenticateBusiness, FoodController.deleteFood);
+
+export default router;
