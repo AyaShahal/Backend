eafe7fbd954da4137502e566ecc6f6ccd0add69c
Aya Shahal <ayashahal055@gmail.com>
2023-05-12 20:23:32 +0300

    user model

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,52 +1,57 @@
 import mongoose from "mongoose";
-const { Schema, model } = mongoose;
 import validator from "validator";
 
+const { Schema, model } = mongoose;
+
 const UserSchema = new Schema(
-    {
-        username: {
-            type: String,
-            required: [true, "Please enter an username"],
-            unique: true,
-        },
-        email: {
-            type: String,
-            required: [true, "Please enter an email"],
-            validate: [validator.isEmail, "Please enter a valid email"],
-            unique: true,
-        },
-        password: {
-            type: String,
-            required: [true, "Please enter an Password"],
-            minlength: [6, "Password must be at least 6 characters long"],
-        },
-        phone: {
-            type: Number,
-            required: true,
-        },
-        Address: [{
-            city: {
-                type: String,
-                required: true,
-            },
-            street: {
-                type: String,
-                required: true,
-            },
-            building: {
-                type: String,
-                required: true,
-            }
-        }],
-     role:{
-        type: String,
-        enum: ["Business", "Organization"]
-     }
+  {
+    username: {
+      type: String,
+      required: [true, "Please enter a username"],
+      unique: true,
     },
-    {
-        collection: "users",
-        timestamps: true,
-    }
+    email: {
+      type: String,
+      required: [true, "Please enter an email"],
+      unique: true,
+      validate: [validator.isEmail, "Please enter a valid email"],
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter a password"],
+      minlength: [6, "Password must be at least 6 characters long"],
+    },
+    phone: {
+      type: Number,
+      required: true,
+    },
+    address: [
+      {
+        city: {
+          type: String,
+          required: true,
+        },
+        street: {
+          type: String,
+          required: true,
+        },
+        building: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+    role: {
+      type: String,
+      enum: ["Business", "Organization"],
+    },
+  },
+  {
+    collection: "users",
+    timestamps: true,
+  }
 );
+
 const User = model("User", UserSchema);
+
 export default User;
